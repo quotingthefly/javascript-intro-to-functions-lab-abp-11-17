ea5e417c4ad12658778a48cd33d42b5f81ec4823
Shana A <shana.arias@ucf.edu>
2017-11-19 07:43:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ function whisper(string) {
   return string.toLowerCase()
 };
 
-function logShout(string) {
-  console.log(`${string.toUpperCase}`)
+funtion logWhisper(string) {
+  console.log(string.toLowerCase())
 };
+
+function sayHiToGrandma(string) {
+  if (string.to)
+}
